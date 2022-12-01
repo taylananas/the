1cb411c6e22aa0fb4b62d16ee74f287db0273f3a
Lojcs <lojcsgit@gmail.com>
2022-12-01 12:25:40 +0300

    Runs the2.py placed into the same folder directly

--- the2tester.py
+++ the2tester.py
@@ -1,7 +1,10 @@
+import sys, subprocess, os
 cases = open("cases.txt")
 answers = open("answers.txt")
 temp = cases.read().splitlines()
-temp2 = answers.read().splitlines()
+temp2 = [*map(float, answers.read().splitlines())]
+cases.close()
+answers.close()
 wrongs = []
 diffs = []
 
@@ -12,56 +15,29 @@ diffs = []
 #      We will not going to include the area of little triangle!
 """
 HOW TO USE:
-COPY YOUR CODE INTO THE def testing() area
-erase any unnecessary print's like you are uploading it to odtuclass
-
-WRITE "inputxt" in the input statement's right side
-for example if your code is like:
-
-inputvalues = input()
-
-CHANGE THIS AS:
-inputvalues = inputxt
-
-and instead of printing the answer, return it
-
-for example:
-
-finalanswer = areaofthegreenzone
-print(finalanswer)
-
-INSTEAD OF PRINT, WRITE:
-return finalanswer
+Copy your the2.py into the same folder
 """
 
 def testing(inputxt):
-    yourinputvariable = inputxt #yourinputvariable = input() instead of input write inputxt
-
-    area = "194.24" #not have to be a string but must be a number with 2 UNROUNDED decimals as the pdf says
-    #print(area) do not do this
-    return area #do this instead
+    if os.path.isfile("the2.py"):
+        area = subprocess.run(f"{sys.executable} the2.py", shell=1 ,input=inputxt, encoding="ascii", stdout=subprocess.PIPE).stdout
+    else:
+        raise FileNotFoundError("Please put the2.py into the same folder.")
+    return float(area) #the2.py should return 2 UNROUNDED decimals as the pdf says
 
 
 def grading(): #a simple check for cases and answers, nothing fancy here
     total = len(temp)
     grade = 0
     for i in range(total):
-        if type(testing(temp[i])) == str:
-            if testing(temp[i]) == temp2[i]:
-                grade +=1
-            else:
-                diff = float(testing(temp[i])) - float(temp2[i])
-                diff = "%.2f"%diff
-                diffs.append(diff)
-                wrongs.append(i+1)
-        elif type(testing(temp[i]))==float:
-            if testing(temp[i]) == float(temp2[i]):
-                grade +=1
-            else:
-                diff = float(testing(temp[i])) - float(temp2[i])
-                diff = "%.2f"%diff
-                diffs.append(diff)
-                wrongs.append(i+1)            
+        tested = testing(temp[i])
+        if abs(tested - temp2[i]) < 0.01:
+            grade +=1
+        else:
+            diff = float(tested) - float(temp2[i])
+            diff = "%.2f"%diff
+            diffs.append(diff)
+            wrongs.append(i+1)         
 
     print(f"{grade}/{total}")
     print(f"Wrong questions are: {wrongs}")
